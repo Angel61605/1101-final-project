5932220d11af90ed253b54c74dfca2dcc480d075
Angel A. <almaae0616@email.com>
2023-11-29 16:31:53 -0600

    Added front-end code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-# First Commit
-print("Hello World!")
-print(1 + 2)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+app.run()
